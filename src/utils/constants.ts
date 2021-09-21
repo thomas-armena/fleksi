a1e0084bb37f5d32708e5f6453cdcc6e55bdff06
Thomas Armena <thomasarmena@gmail.com>
2021-09-20 21:03:05 -0400

    balanced upgrades

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,8 @@
 import path from "path";
 
-export const WORKING_DIR = path.resolve(process.cwd());
 export const APP_DIR = path.resolve(__dirname);
+export const WORKING_DIR = path.resolve(process.cwd(), 'example');
+
 
 export const THING_APP_REGEX = '<thing-app>';
 export const PATH_TO_HTML_TEMPLATE = path.join(APP_DIR, '..', '..', 'src', 'server', 'template.html');
